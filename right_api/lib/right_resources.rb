e5e73238ca4bf0f23b9ae398e7f36911ba44ff93
Satoshi Ohki <roothybrid7@gmail.com>
2010-10-21 08:38:39 +0900

    modified

--- right_api/lib/right_resources.rb
+++ right_api/lib/right_resources.rb
@@ -1,58 +1,87 @@
 #!/usr/bin/env ruby
 # @author Satoshi Ohki
 
+require 'rubygems'
+require 'rest_client'
+
 module RightResource
 end
 
 module RightResource
 class Connection
-  def initialize(api, params={})
-    @api = api
+  attr_accessor :api_version, :log, :api, :format
+  attr_reader :headers
+
+  def initialize(api, format="xml")
+    @api_version = "1.0" if @api_version.nil?
+    @api = api ||= "https://my.rightscale.com/api/acct/"
+    login(params) unless params.nil?
+    @format = format
   #  api.log_file = RSAPI_LOG
   #  api.log = true
   end
 
   def login(params={})
-    @api.login(
-      :username => params[:username],
-      :password => params[:password],
-      :account => params[:account]
-    ) || #TODO: Exception "Not login!!"
+    @username = params[:username]
+    @password = params[:password]
+    @account = params[:account]
+
+    if @account.match(/[0-9]+/)
+      @api += @account
+    else
+      raise "Invalid account id"
+    end
+    @api_object = RestClient::Resource.new(@api_call, @username, @password)
+  rescue => e
+    STDERR::puts e.message
+    exit 0
   end
 
-  def send(path, method="get", headers={})
-    response = @api.send(path, method, headers)
+  def send(path, method=:get, headers={})
+    if /\?(.*)$/ =~ path
+      path = URI.encode("#{path}&format=#{@format}")
+    else
+      path = URI.encode("#{path}?format=#{@format}")
+    end
+    unless method.match(/(:get|:put|:post|:delete)/)
+      raise "Invalid Action: get|put|post|delete only"
+    end
+    api_version = {:x_api_version => @api_version, :api_version => @api_version}
+    @response = @api[path].send(method, api_version.merge(headers))
+    @headers = @response.headers ||= {}
+    @resource_id = @headers[:location].match(/\d+$/)
+
+    @response.body
+  rescue => e
+    STDERR::puts e.message
+    exit 0
   end
 
   # show|index
   def get(path, headers={})
-    @api.send(path, "get", headers)
+    @api.send(path, :get, headers)
   end
 
   # create
   def post(path, headers={})
-    @api.send("", "post", params)
+    @api.send(path, :post, params)
   end
 
   # update
   def put(path, headers={})
-    @api.send("", "put", params)
+    @api.send(path, :put, params)
   end
 
   # destory
   def delete(path, headers={})
-    @api.send("", "delete", params)
-  end
-
-  def head
-    @api.headers || {}
+    @api.send(path, :delete, params)
   end
 end
 
 class Base
   class << self
-    def connection
-      @connection = Connection.new(api, format)
+    def connection(refresh=false)
+      @connection = Connection.new(api, format) if refresh || @connection.nil?
       @connection.login(:username => user, :password => pass, :account => account)
     end
     def headers
