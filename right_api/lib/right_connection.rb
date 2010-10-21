b315d4e751944c28250920b2529838101cbb3357
Satoshi Ohki <roothybrid7@gmail.com>
2010-10-21 14:36:26 +0900

    RightScaleAPI test: get servers

--- right_api/lib/right_connection.rb
+++ right_api/lib/right_connection.rb
@@ -8,15 +8,12 @@ require 'rest_client'
 module RightResource
   class Connection
     attr_accessor :api_version, :log, :api, :format
-    attr_reader :headers
+    attr_reader :headers, :resource_id
 
-    def initialize(api, format="xml")
-      @api_version = "1.0" if @api_version.nil?
-      @api = api ||= "https://my.rightscale.com/api/acct/"
-      login(params) unless params.nil?
-      @format = format
-    #  api.log_file = RSAPI_LOG
-    #  api.log = true
+    def initialize(params={})
+      @api_version = params[:version] ||= "1.0"
+      @api = params[:api] ||= "https://my.rightscale.com/api/acct/"
+      @format = params[:format].nil? ? "xml" : params[:format]
     end
 
     def login(params={})
@@ -24,55 +21,51 @@ module RightResource
       @password = params[:password]
       @account = params[:account]
 
-      if @account.match(/[0-9]+/)
-        @api += @account
-      else
-        raise "Invalid account id"
-      end
-      @api_object = RestClient::Resource.new(@api_call, @username, @password)
+      @api = @api + @account
+      @api_object = RestClient::Resource.new(@api, @username, @password)
     rescue => e
-      STDERR::puts e.message
+      STDERR.puts e.message
       exit 0
     end
 
-    def send(path, method=:get, headers={})
+    def send(path, method="get", headers={})
       if /\?(.*)$/ =~ path
         path = URI.encode("#{path}&format=#{@format}")
       else
         path = URI.encode("#{path}?format=#{@format}")
       end
-      unless method.match(/(:get|:put|:post|:delete)/)
+      unless method.match(/(get|put|post|delete)/)
         raise "Invalid Action: get|put|post|delete only"
       end
       api_version = {:x_api_version => @api_version, :api_version => @api_version}
-      @response = @api[path].send(method, api_version.merge(headers))
+      @response = @api_object[path].send(method.to_sym, api_version.merge(headers))
       @headers = @response.headers ||= {}
-      @resource_id = @headers[:location].match(/\d+$/)
+      @resource_id = @headers[:location].match(/\d+$/) unless @headers[:location].nil?
 
       @response.body
     rescue => e
-      STDERR::puts e.message
+      STDERR.puts e.message
       exit 0
     end
 
     # show|index
     def get(path, headers={})
-      @api.send(path, :get, headers)
+      self.send(path, "get", headers)
     end
 
     # create
     def post(path, headers={})
-      @api.send(path, :post, params)
+      self.send(path, "post", params)
     end
 
     # update
     def put(path, headers={})
-      @api.send(path, :put, params)
+      self.send(path, "put", params)
     end
 
     # destory
     def delete(path, headers={})
-      @api.send(path, :delete, params)
+      self.send(path, "delete", params)
     end
   end
 end
---
+++ right_api/test/servers_index.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'yaml'
+
+$LOAD_PATH << File::expand_path(File::dirname(__FILE__) + "/../")
+p $LOAD_PATH
+
+require "lib/right_connection.rb"
+
+puts "BEGIN OF SCRIPT"
+auth_data = open(File::expand_path(File::dirname(__FILE__)) + "/api_auth.yml") {|f| YAML.load(f)}
+# convert key.type: String2Symbol
+login_params = {}
+auth_data.each_pair {|key,value| login_params[key.to_sym] = value}
+p login_params.inspect
+c1 = RightResource::Connection.new
+p c1.inspect
+c2 = RightResource::Connection.new(:format => "js")
+p c2.inspect
+c2.login(login_params)
+p c2.inspect
+#res = c2.get("servers")
+#puts res
+puts "Get RightScale API info: for xml(Default)"
+res = c1.get("servers?filter=nickname=kff-mixi-db-001")
+puts res
+puts "Get RightScale API info: for json"
+res = c2.get("servers?filter=nickname=kff-mixi-db-001")
+puts res
+puts "Get RightScale API info: for xml"
+c2.format = "xml"
+res = c2.get("servers?filter=nickname=kff-mixi-db-001")
+puts res
+
+puts "END OF SCRIPT"
