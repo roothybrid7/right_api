c0625c6a49edaacbbf34acb6c349aee5c554f375
Satoshi Ohki <roothybrid7@gmail.com>
2010-10-28 16:28:23 +0900

    Delete gabege

--- rightresource/lib/right_resource/deployment.rb
+++
@@ -1,1 +0,0 @@
-class Deployment < RightResource::Base; end
--- rightresource/lib/right_resource/formats.rb
+++
@@ -1,7 +0,0 @@
-module RightResource
-  module Formats
-    autoload :XmlFormat, 'right_resource/formats/xml_format'
-    autoload :JsonFormat, 'right_resource/formats/json_format'
-  end
-end
-
